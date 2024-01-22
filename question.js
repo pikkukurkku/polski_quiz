d305f32d821011c1658d25eb3ab456597984ecfe
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-22 14:33:34 +0100

    html and css mainly done

--- question.js
+++ question.js
@@ -1,16 +1,17 @@
 class Question {
-    constructor(text, choices, answer) {
-      this.text = text;
-      this.choices = choices;
-      this.answer = answer;
-    }
-    shuffleChoices() {
-      const shuffled = [];
-      while (this.choices.length) {
-        const random = Math.floor(Math.random() * this.choices.length);
-        shuffled.push(this.choices.splice(random, 1)[0]);
-      }
-      this.choices = shuffled;
-    }
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
   }
-  
+  shuffleChoices() {
+    const shuffled = [];
+    while (this.choices.length) {
+      const random = Math.floor(Math.random() * this.choices.length);
+      shuffled.push(this.choices.splice(random, 1)[0]);
+    }
+    this.choices = shuffled;
+  }
+}
+
+console.log("Hello World");
