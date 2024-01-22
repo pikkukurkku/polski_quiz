92648f857413962e404b25304192d39ddba102f7
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-22 12:22:49 +0100

    pre-work

---
+++ script.js
@@ -0,0 +1,32 @@
+/************  QUIZ DATA  ************/
+
+  // Array with the quiz questions
+  const questions = [
+    new Question(
+        "Kto to jest?",
+        ["Messi", "Lewandowski", "Mbappe"],
+        2
+    ),
+        new Question(
+      "Co to jest?",
+      ["Ogorek", "Pomidor", "Banan"],
+      1
+    ),
+    new Question(
+      "Co to jest?",
+      ["Dom", "Hus", "Kuca"],
+      1
+    ),
+    new Question(
+      "The capital of Polska is...",
+      ["Krakow", "Budapest", "Warszawa"],
+      3
+    ),
+    new Question(
+        "Co to jest?",
+        ["Komputador", "Komputer", "Kalkulator"],
+        2
+      ),
+
+    // Add more questions here
+  ];
